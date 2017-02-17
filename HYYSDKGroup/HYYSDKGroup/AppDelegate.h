3cd6e8766471e763aba5205770f0ed2d76f0fc73
ABC_Show <361569679@qq.com>
2017-02-17 17:17:42 +0800

    项目创建

---
+++ HYYSDKGroup/HYYSDKGroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYYSDKGroup
+//
+//  Created by xuchunlei on 2017/2/17.
+//  Copyright © 2017年 abc_show. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
